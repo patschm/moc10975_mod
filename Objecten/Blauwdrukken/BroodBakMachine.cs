b2c13b23f1f4a3608b5ca47e6e6c4d8ed8994ee7
Patrick <pschmidt@pasit.nl>
2021-03-11 11:30:34 +0100

    Blauwdruk

---
+++ Objecten/Blauwdrukken/BroodBakMachine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blauwdrukken
+{
+    // Blauwdruk van de broodbakmachine
+    class BroodBakMachine
+    {
+        // Field. Hierin sla je eigenschappen op
+        private int _bakTijd = 120;
+
+        // Property. Hiermee controleer ik de waardes die aan een field mogen worden toegekend
+        public int BakTijd
+        {
+            get
+            {
+                return _bakTijd;
+            }
+            set
+            {
+                if (value >= 0 && value < 250)
+                {
+                    _bakTijd = value;
+                }
+            }
+        }
+        // Auto-generating property. Genereert zijn eigen private field
+        public int BakTemperatuur { get; set; }
+
+        // Gedrag. Leg je vast in functies of procedures (Method)
+        public void Bak()
+        {
+            Console.WriteLine($"Check temperatuur ({BakTemperatuur} gr C)");
+            if (BakTemperatuur >= 160)
+            {
+                Console.WriteLine($"We bakken nu {BakTijd} minuten");
+            }
+        }
+
+        // Constructor
+        // Gebruik je om _fields_ van een initiele waarde te voorzien
+        public BroodBakMachine(int tijd)
+        {
+            BakTijd = tijd;
+        }
+        public BroodBakMachine()
+        {
+
+        }
+    }
+
+
+}
---
+++ Objecten/Blauwdrukken/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Blauwdrukken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BroodBakMachine bbm1 = new BroodBakMachine(-123);
+            BroodBakMachine bbm2 = new BroodBakMachine { BakTijd = 90, BakTemperatuur=200 };
+
+            bbm1.BakTemperatuur = 170;
+            bbm1.BakTijd = 170;
+            bbm1.Bak();
+            
+        }
+    }
+}
